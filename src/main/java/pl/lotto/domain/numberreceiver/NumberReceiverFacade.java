19ed703ceeba331428d1361f6ffbbbfca287a46b
Mateusz <matpia98@gmail.com>
2024-05-14 11:25:19 +0200

    moved facade methods to separate class, dto, improved test

--- src/main/java/pl/lotto/domain/numberreceiver/NumberReceiverFacade.java
+++ src/main/java/pl/lotto/domain/numberreceiver/NumberReceiverFacade.java
@@ -1,4 +1,27 @@
 package pl.lotto.domain.numberreceiver;
 
+import lombok.AllArgsConstructor;
+import pl.lotto.domain.numberreceiver.dto.InputNumberResultDto;
+
+import java.util.Set;
+
+@AllArgsConstructor
 public class NumberReceiverFacade {
+
+    private final NumberValidator validator;
+
+    public InputNumberResultDto inputNumbers(Set<Integer> numbersFromUser){
+        boolean areAllNumbersInRange = validator.areAllNumbersInRange(numbersFromUser);
+        if (areAllNumbersInRange) {
+            return InputNumberResultDto.builder()
+                    .message("success")
+                    .build();
+        }
+        return InputNumberResultDto.builder()
+                .message("failed")
+                .build();
+    }
+
+
+
 }
--- src/test/java/pl/lotto/domain/numberreceiver/NumberReceiverFacadeTest.java
+++ src/test/java/pl/lotto/domain/numberreceiver/NumberReceiverFacadeTest.java
@@ -1,7 +1,65 @@
 package pl.lotto.domain.numberreceiver;
 
+import org.junit.jupiter.api.Test;
+import pl.lotto.domain.numberreceiver.dto.InputNumberResultDto;
+
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class NumberReceiverFacadeTest {
+
+    NumberReceiverFacade numberReceiverFacade = new NumberReceiverFacade(
+            new NumberValidator()
+    );
+
+    @Test
+    public void should_return_success_when_user_gave_six_numbers() {
+        // given
+        Set<Integer> numbersFromUser = Set.of(1,2,3,4,5,6);
+
+        // when
+        InputNumberResultDto result = numberReceiverFacade.inputNumbers(numbersFromUser);
+
+        // then
+        assertThat(result.message()).isEqualTo("success");
+    }
+
+    @Test
+    public void should_return_failed_when_user_gave_at_least_one_number_out_of_range_of_1_to_99() {
+        // given
+        Set<Integer> numbersFromUser = Set.of(1, 2000, 3, 4, 5, 6);
+
+        // when
+        InputNumberResultDto result = numberReceiverFacade.inputNumbers(numbersFromUser);
+
+        // then
+        assertThat(result.message()).isEqualTo("failed");
+    }
+
+    @Test
+    public void should_return_failed_when_user_gave_less_than_six_numbers() {
+        // given
+        Set<Integer> numbersFromUser = Set.of(1, 3, 4, 5, 6);
+
+        // when
+        InputNumberResultDto result = numberReceiverFacade.inputNumbers(numbersFromUser);
+
+        // then
+        assertThat(result.message()).isEqualTo("failed");
+    }
+
+    @Test
+    public void should_return_failed_when_user_gave_more_than_six_numbers() {
+        // given
+        Set<Integer> numbersFromUser = Set.of(1, 2, 3, 4, 5, 6, 7);
+
+        // when
+        InputNumberResultDto result = numberReceiverFacade.inputNumbers(numbersFromUser);
+
+        // then
+        assertThat(result.message()).isEqualTo("failed");
+    }
 
 }
