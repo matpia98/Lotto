d747e0d186294a4b46a4e51dcba409dd2511ed74
Mateusz <matpia98@gmail.com>
2024-05-16 20:27:08 +0200

    implemented result checker module with tests

--- src/main/java/pl/lotto/domain/resultannouncer/ResultAnnouncer.java
+++
@@ -1,4 +0,0 @@
-package pl.lotto.domain.resultannouncer;
-
-public class ResultAnnouncer {
-}
---
+++ src/main/java/pl/lotto/domain/resultannouncer/ResultAnnouncerConfiguration.java
@@ -0,0 +1,12 @@
+package pl.lotto.domain.resultannouncer;
+
+import pl.lotto.domain.resultchecker.ResultCheckerFacade;
+
+import java.time.Clock;
+
+public class ResultAnnouncerConfiguration {
+
+    ResultAnnouncerFacade createForTest(ResultCheckerFacade resultCheckerFacade, ResponseRepository responseRepository, Clock clock) {
+        return new ResultAnnouncerFacade(resultCheckerFacade, responseRepository, clock);
+    }
+}
